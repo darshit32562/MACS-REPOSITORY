4dd7e15a27cc32bab57f439527877cd632489a1d
darshit32562 <darshitagg2008@gmail.com>
2026-03-24 21:16:52 +0530

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -1,33 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int iterations_m = 0;
-void merge(vector<int>& arr, int left, int mid, int right) {
+
+int mergeIterations = 0;
+
+void mergeArray(vector<int>& arr, int l, int m, int r) {
     vector<int> temp;
-    int i = left, j = mid + 1;
-    while(i <= mid && j <= right) {
-        iterations_m++;
-        if(arr[i] <= arr[j]) {
+    int i = l, j = m + 1;
+
+    while(i <= m && j <= r) {
+        mergeIterations++;
+        if(arr[i] <= arr[j])
             temp.push_back(arr[i++]);
-        } else {
+        else
             temp.push_back(arr[j++]);
-        }
     }
-    while(i <= mid) temp.push_back(arr[i++]);
-    while(j <= right) temp.push_back(arr[j++]);
-    for(int k = 0; k < temp.size(); k++) {
-        arr[left + k] = temp[k];
-    }
+
+    while(i <= m) temp.push_back(arr[i++]);
+    while(j <= r) temp.push_back(arr[j++]);
+
+    for(int k = 0; k < temp.size(); k++)
+        arr[l + k] = temp[k];
 }
-void mergesort1(vector<int>& arr, int left, int right) {
-    if(left < right) {
-        int mid = (left + right) / 2;
-        mergesort1(arr, left, mid);
-        mergesort1(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
+
+void mergeSortHelper(vector<int>& arr, int l, int r) {
+    if(l >= r) return;
+
+    int m = (l + r) / 2;
+    mergeSortHelper(arr, l, m);
+    mergeSortHelper(arr, m + 1, r);
+    mergeArray(arr, l, m, r);
 }
+
 int mergeSort(vector<int>& arr) {
-    iterations_m = 0;
-    mergesort1(arr, 0, arr.size() - 1);
-    return iterations_m;
+    mergeIterations = 0;
+    mergeSortHelper(arr, 0, arr.size() - 1);
+    return mergeIterations;
 }
