2cc5d744a783407629f4151962a92785b5fefe0f
darshit32562 <darshitagg2008@gmail.com>
2026-03-24 21:36:34 +0530

    Update quick.cpp

--- quick.cpp
+++ quick.cpp
@@ -1,34 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int quickIterations = 0;
-
-int partitionArray(vector<int>& arr, int low, int high) {
+int partitionArray(vector<int>& arr, int low, int high, int &iterations) {
     int pivot = arr[high];
     int i = low - 1;
 
     for(int j = low; j < high; j++) {
-        quickIterations++;
+        iterations++;
         if(arr[j] < pivot) {
             i++;
             swap(arr[i], arr[j]);
         }
     }
 
-    swap(arr[i+1], arr[high]);
-    return i+1;
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
 }
 
-void quickSortHelper(vector<int>& arr, int low, int high) {
+void quickSortHelper(vector<int>& arr, int low, int high, int &iterations) {
     if(low < high) {
-        int pi = partitionArray(arr, low, high);
-        quickSortHelper(arr, low, pi - 1);
-        quickSortHelper(arr, pi + 1, high);
+        int pi = partitionArray(arr, low, high, iterations);
+        quickSortHelper(arr, low, pi - 1, iterations);
+        quickSortHelper(arr, pi + 1, high, iterations);
     }
 }
 
 int quickSort(vector<int>& arr) {
-    quickIterations = 0;
-    quickSortHelper(arr, 0, arr.size() - 1);
-    return quickIterations;
+    int iterations = 0;
+    quickSortHelper(arr, 0, arr.size() - 1, iterations);
+    return iterations;
 }
