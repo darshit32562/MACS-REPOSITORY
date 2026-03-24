26951bc3fbf1b00067abbf9e21978b21f815cac6
darshit32562 <darshitagg2008@gmail.com>
2026-03-24 21:18:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,67 +4,47 @@ using namespace std;
 #include "bubble.cpp"
 #include "insertion.cpp"
 #include "selection.cpp"
-#include "qs.cpp"
+#include "quick.cpp"
 #include "merge.cpp"
 
-void printArray(vector<int>& arr) {
+void printArray(const vector<int>& arr) {
     for(int x : arr) cout << x << " ";
     cout << endl;
 }
 
-int main() {
-    vector<int> arr = {77 , 71 , 51 , 21 , 109 , 557 , 654 , 7 , 93 , 89 , 34 , 1008 , 2708 , 2007 , 79 , 87 , 33 , 8};
-    cout << "Original Array: ";
-    printArray(arr);
-    vector<int> temp;
-    // Bubble Sort
-    temp = arr;
+vector<int> generateArray(int n) {
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++)
+        arr[i] = rand() % 1000;
+    return arr;
+}
+
+void testSort(string name, vector<int> arr, int (*sortFunc)(vector<int>&)) {
     auto start = chrono::high_resolution_clock::now();
-    int it = bubbleSort(temp);
+    int it = sortFunc(arr);
     auto end = chrono::high_resolution_clock::now();
-    cout << "\nBubble Sort: ";
-    printArray(temp);
-    cout << "Iterations: " << it;
-    cout << " | Time: " << chrono::duration<double, milli>(end-start).count() << " ms\n";
-    // Insertion Sort
-    temp = arr;
-    start = chrono::high_resolution_clock::now();
-    it = insertionSort(temp);
-    end = chrono::high_resolution_clock::now();
-    cout << "\nInsertion Sort: ";
-    printArray(temp);
-    cout << "Iterations: " << it;
-    cout << " | Time: " << chrono::duration<double, milli>(end-start).count() << " ms\n";
 
-    // Selection Sort
-    temp = arr;
-    start = chrono::high_resolution_clock::now();
-    it = selectionSort(temp);
-    end = chrono::high_resolution_clock::now();
-    cout << "\nSelection Sort: ";
-    printArray(temp);
+    cout << "\n=== " << name << " ===\n";
+    printArray(arr);
     cout << "Iterations: " << it;
-    cout << " | Time: " << chrono::duration<double, milli>(end-start).count() << " ms\n";
+    cout << " | Time: "
+         << chrono::duration<double, milli>(end-start).count()
+         << " ms\n";
+}
 
-    // Quick Sort
-    temp = arr;
-    start = chrono::high_resolution_clock::now();
-    it = quickSort(temp);
-    end = chrono::high_resolution_clock::now();
-    cout << "\nQuick Sort: ";
-    printArray(temp);
-    cout << "Iterations: " << it;
-    cout << " | Time: " << chrono::duration<double, milli>(end-start).count() << " ms\n";
+int main() {
+    srand(time(0));
 
-    // Merge Sort
-    temp = arr;
-    start = chrono::high_resolution_clock::now();
-    it = mergeSort(temp);
-    end = chrono::high_resolution_clock::now();
-    cout << "\nMerge Sort: ";
-    printArray(temp);
-    cout << "Iterations: " << it;
-    cout << " | Time: " << chrono::duration<double, milli>(end-start).count() << " ms\n";
+    vector<int> arr = generateArray(20);
+
+    cout << "Original Array:\n";
+    printArray(arr);
+
+    testSort("Bubble Sort", arr, bubbleSort);
+    testSort("Insertion Sort", arr, insertionSort);
+    testSort("Selection Sort", arr, selectionSort);
+    testSort("Quick Sort", arr, quickSort);
+    testSort("Merge Sort", arr, mergeSort);
 
     return 0;
 }
