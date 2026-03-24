63fbc81f72ae93fc6c95f60000a31b050fe4a36d
darshit32562 <darshitagg2008@gmail.com>
2026-03-24 21:36:17 +0530

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int selectionSort(vector<int>& arr) {
     int n = arr.size();
     int iterations = 0;
-    for(int i = 0; i < n-1; i++) {
+
+    for(int i = 0; i < n - 1; i++) {
         int minIndex = i;
-        for(int j = i+1; j < n; j++) {
+
+        for(int j = i + 1; j < n; j++) {
             iterations++;
             if(arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
+
         swap(arr[i], arr[minIndex]);
     }
+
     return iterations;
 }
